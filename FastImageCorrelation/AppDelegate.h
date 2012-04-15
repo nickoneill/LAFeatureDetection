a35712e9a5ca02f2141a9ba9c6f855893e073d9b
Nick O'Neill <nick.oneill@gmail.com>
2012-04-15 15:07:16 -0700

    Initial Commit

---
+++ FastImageCorrelation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FastImageCorrelation
+//
+//  Created by Nick O'Neill on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FastImageCorrelation/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  FastImageCorrelation
+//
+//  Created by Nick O'Neill on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
