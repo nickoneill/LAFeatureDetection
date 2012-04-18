298f1c420d1c32cdb0b33dd66368e9d502cffbe4
Nick O'Neill <nick.oneill@gmail.com>
2012-04-18 15:16:46 -0700

    Adding more files.

---
+++ NOImageCorrelate.h
@@ -0,0 +1,13 @@
+//
+//  NOImageCorrelate.h
+//  FastImageCorrelation
+//
+//  Created by Nick O'Neill on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NOImageCorrelate : NSObject
+
+@end
---
+++ NOImageCorrelate.m
@@ -0,0 +1,13 @@
+//
+//  NOImageCorrelate.m
+//  FastImageCorrelation
+//
+//  Created by Nick O'Neill on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NOImageCorrelate.h"
+
+@implementation NOImageCorrelate
+
+@end
